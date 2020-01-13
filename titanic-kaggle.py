44cba3129afa908b792dd7fd7357452deba9cb6c
malay <tomalaypandey.tanu@gmail.com>
2020-01-14 00:16:00 +0530

    Applied svm

--- titanic-kaggle.py
+++ titanic-kaggle.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sklearn
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('titanic/train.csv')
 
@@ -33,10 +34,35 @@ dataset['Sex'] = labelEncoder.fit_transform(dataset['Sex'])
 dataset['Embarked'] = labelEncoder.fit_transform(dataset['Embarked'])
 
 dataset = dataset.values
-one_hot_encoder = OneHotEncoder(categorical_features=[1,7])
+one_hot_encoder = OneHotEncoder(categorical_features=[1, 7])
 dataset = one_hot_encoder.fit_transform(dataset).toarray()
 
 # Applying Algos
+x_train, x_test, y_train, y_test = train_test_split(dataset[:, 1:], dataset[:, 1], test_size=0.2)
+
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='poly', random_state=0)
+classifier.fit(x_train, y_train)
+
+yPred_svm = classifier.predict(x_test)
+acc_sr = accuracy_score(yPred_svm, y_test)
+
+# Test Dataset and Processing
+
+testDataset = pd.read_csv('titanic/test.csv')
+testDataset = testDataset.drop(['PassengerId', 'Ticket', 'Cabin', 'Name'], axis=1)
+testDataset['Age'] = testDataset['Age'].fillna(testDataset['Age'].mean())
+testDataset['Fare'] = testDataset['Fare'].fillna(testDataset['Fare'].mean())
+
+labelEncoder2 = LabelEncoder()
+testDataset['Sex'] = labelEncoder2.fit_transform(testDataset['Sex'])
+testDataset['Embarked'] = labelEncoder2.fit_transform(testDataset['Embarked'])
+
+testDataset = testDataset.values
+ohe = OneHotEncoder(categorical_features=[0, 6])
+testDataset = ohe.fit_transform(testDataset).toarray()
+
 
 
 
